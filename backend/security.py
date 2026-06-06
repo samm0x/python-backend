c097d88f3a0e77e0781ac415b145408cfabbf1a5
samm0x <samm.ch.009@gmail.com>
2026-06-06 07:29:13 -0700

    fix

--- backend/security.py
+++ backend/security.py
@@ -8,10 +8,14 @@ from sqlalchemy.orm import Session
 from jose import JWTError, jwt
 from fastapi import HTTPException , Request
 from .models import User, TokenBlacklist , AuditLog
-from .config import SECRET_KEY, ALGORITHM
+from backend.config import settings
 from .database import get_db
 import hashlib
 from time import time
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
