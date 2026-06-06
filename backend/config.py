8aa58c8bda8396ebe190181571edc3f2d418ede1
samm0x <samm.ch.009@gmail.com>
2026-06-06 07:21:02 -0700

    add env and config

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,12 @@
-SECRET_KEY = "super-secret-key-change-this"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    DATABASE_URL: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
