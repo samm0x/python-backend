cfaf380999aab633568bc890ed5437a088162b0c
samm0x <samm.ch.009@gmail.com>
2026-06-07 12:39:18 -0700

    fix

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -1,8 +1,20 @@
 from celery import Celery
+import time
+
 celery = Celery(
     "tasks",
-    broker = "redid://localhost:6379/0"
+    broker="redis://localhost:6379/0",
+    backend="redis://localhost:6379/0"
 )
+
 @celery.task
-def send_email():
-    print("email sent")
+def send_email(username : str):
+    time.sleep(3)
+    print(f"email sent to {username}")
+    return {"status": "email sent", "to": username}
+
+@celery.task
+def generate_report(user_id : str):
+    time.sleep(5)
+    print(f"report generated for user {user_id}")
+    return {"status": "report generated", "to": user_id}
